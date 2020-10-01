a34ae5970a143e0dc29c10f2a27256d869893446
github wubuchako <jfdev12@gmail.com>
2020-10-01 10:59:26 +0900

    reset and result

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
 const computerChoiceEl = document.getElementById('computerChoice');
-const resultText = document.getElementById('resultText');
 
 const playerRock = document.getElementById('playerRock');
 const playerPaper = document.getElementById('playerPaper');
@@ -14,16 +13,17 @@ const computerPaper = document.getElementById('computerPaper');
 const computerScissors = document.getElementById('computerScissors');
 
 const allGameIcons = document.querySelectorAll('.far');
+const resultText = document.getElementById('resultText');
 
 const choices = {
-  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
-  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
-  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
-  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
-  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+  rock: { name: 'Rock', defeats: ['scissors'] },
+  paper: { name: 'Paper', defeats: ['rock'] },
+  scissors: { name: 'Scissors', defeats: ['paper'] },
 };
 
-console.log(allGameIcons);
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
 
 // Reset all 'selected' icons
 function resetSelected(){
@@ -32,9 +32,81 @@ function resetSelected(){
    });
 }
 
+// ResetScore & playerChoice / computerChoice
+function resetAll(){
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  playerScoreEl.textContent = playerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  playerChoiceEl.textContent = '';
+  computerChoiceEl.textContent = '';
+  resultText.textContent = '';
+  resetSelected();
+}
+
+// Random computer choice
+function computerRandomChoice(){
+  const computerChoiceNumber = Math.random();
+  if(computerChoiceNumber < 0.3){
+    computerChoice = 'rock';
+  } else if(computerChoiceNumber <= 0.6){
+    computerChoice = 'paper';
+  } else {
+    computerChoice = 'scissors';
+  }
+}
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    default:
+      break;
+  }
+ }
+
+// Check result, increase scores, update resultText
+function updateScore(playerChoice){
+  if(playerChoice === computerChoice){
+    resultText.textContent = "It's a tie."; 
+  } else {
+    const choice = choices[playerChoice];
+    console.log(choice.defeats.indexOf(computerChoice));
+    if(choice.defeats.indexOf(computerChoice) > -1){
+      resultText.textContent = "You won!";
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultText.textContent = "You Lost!";
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+
+  }
+}
+
+// Call functions to process turn
+function checkResult(playerChoice){
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice);
+}
+
 // Passing player selection value and styling icons
 function select(playerChoice) {
-   resetSelected();
+   checkResult(playerChoice);
   // Add 'selected' styling & playerChoice
   switch (playerChoice) {
     case 'rock':
@@ -54,3 +126,5 @@ function select(playerChoice) {
   }
 }
 
+// On stratup, set initial values
+resetAll();
